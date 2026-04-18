41af6a2b92df241550cd4b7bb9876829cbaf6cc7
Wei WEI <pokifox@hotmail.com>
2026-04-18 08:59:42 +0800

    revert

--- miniapp/lib/main.dart
+++ miniapp/lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:flutter/material.dart';
+
 import 'package:mpflutter_core/mpflutter_core.dart';
 import 'package:mpflutter_wechat_api/mpflutter_wechat_api.dart';
 
+import 'package:device_preview/device_preview.dart';
+
 import 'package:qiuzhijia/app.dart';
+import 'package:qiuzhijia/tapah/reserved.dart' as tapah;
 
 void main() {
-	runMPApp(const MainApp());
+	runApp(DevicePreview(
+		enabled: true,
+		defaultDevice: tapah.deviceinfo,
+		builder: (context) => const MainApp(),
+	),);
 
 	if (kIsMPFlutter) {
 		try {
